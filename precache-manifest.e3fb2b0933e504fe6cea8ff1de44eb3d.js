89cba0c7245fb439ee7481848f5b8a53a565f270
DESKTOP-84M71MF\furkan <furkankaplan@gmail.com>
2019-09-22 16:31:37 +0300

    Updates

---
+++ precache-manifest.e3fb2b0933e504fe6cea8ff1de44eb3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb8751493578739ae30e6b7ead488c2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ebd7ecd4c5d5c067637b",
+    "url": "/monster-rolodex/static/css/main.8f576f3f.chunk.css"
+  },
+  {
+    "revision": "aaf825358dba75e9d19f",
+    "url": "/monster-rolodex/static/js/2.bd50b089.chunk.js"
+  },
+  {
+    "revision": "ebd7ecd4c5d5c067637b",
+    "url": "/monster-rolodex/static/js/main.8c684224.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
